01f9cf090168780c51028f7eb29cd29945fc9aac
Petr Viktorin <encukou@gmail.com>
2018-12-30 14:08:08 +0100

    Drop Python 2 support; target Python 3.6+

--- setup.py
+++ setup.py
@@ -31,11 +31,9 @@ setup(
             Intended Audience :: Developers
             License :: OSI Approved :: MIT License
             Operating System :: OS Independent
-            Programming Language :: Python :: 2
-            Programming Language :: Python :: 2.6
-            Programming Language :: Python :: 2.7
             Programming Language :: Python :: 3
-            Programming Language :: Python :: 3.3
+            Programming Language :: Python :: 3.6
+            Programming Language :: Python :: 3.7
             Topic :: Games/Entertainment
         """.splitlines() if x.strip()],
     install_requires=[
